deb93470fa92737345ecd10cf927028951368cea
Unknown <daniel_wolan@o2.pl>
2017-12-08 21:07:50 +0100

    set route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get 'admin/index'
+  # get 'admin/index'
+  #
+  # get 'admin/show'
+  #
+  # get 'admin/new'
+  #
+  # get 'admin/edit'
+  #
+  # get 'admin/delete'
 
-  get 'admin/show'
-
-  get 'admin/new'
-
-  get 'admin/edit'
-
-  get 'admin/delete'
+  match ':controller(/:action(/:id))', :via => [:get, :post]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
