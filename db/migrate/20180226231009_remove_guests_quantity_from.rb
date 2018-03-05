483d6232367aeb1611fe218c4c11f6994e13ba6a
Unknown <daniel_wolan@o2.pl>
2018-03-05 22:08:32 +0100

    delete unnecessary migration

--- db/migrate/20180226231009_remove_guests_quantity_from.rb
+++
@@ -1,9 +0,0 @@
-class RemoveGuestsQuantityFrom < ActiveRecord::Migration[5.1]
-  def up
-    remove_column :bookings, :guests_quantity, :string
-  end
-
-  def down
-    add_column :bookings, :guests_quantity, :string
-  end
-end
